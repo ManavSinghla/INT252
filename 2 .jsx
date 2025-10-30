a769c8f363191b762ff05a265390a8e3736962ac
Manav Singla <manavsinghla123@gmail.com>
2025-10-30 11:49:07 +0530

    30/10/25

---
+++ 2 .jsx
@@ -0,0 +1,12 @@
+// Command to run program
+// npm init vite my-vite-project
+
+function App(){
+    const[count,setCount]=React.useState(0);
+    return(
+        <>
+        <div> </div>
+        <h1>Manav</h1>
+        </>
+    )
+}
