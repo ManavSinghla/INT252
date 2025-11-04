26ef7602430b8e74894611ccfd806b30600e8b00
Manav Singla <manavsinghla123@gmail.com>
2025-11-04 10:44:59 +0530

    04/11/25

--- 2 .jsx
+++ 2 .jsx
@@ -1,12 +1,15 @@
 // Command to run program
 // npm init vite my-vite-project
 
-function App(){
-    const[count,setCount]=React.useState(0);
-    return(
-        <>
-        <div> </div>
+function App() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <>
+      <div>
         <h1>Manav</h1>
-        </>
-    )
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    </>
+  );
 }
